6ef31f585622ad92dea11ca17d870f019f5b1586
EC2 Default User <ec2-user@ip-172-31-94-72.ec2.internal>
2024-01-26 16:55:39 +0000

    commiting from Cloud9

--- bikeshare_model/pipeline.py
+++ bikeshare_model/pipeline.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
@@ -15,26 +16,63 @@ from bikeshare_model.processing.features import Mapper
 from bikeshare_model.processing.features import OutlierHandler
 from bikeshare_model.processing.features import WeekdayOneHotEncoder
 
-bikeshare_pipe=Pipeline([
-
-    ('weathersit_imputation', WeathersitImputer(variables=config.model_config.weathersit_var)),
-    ('weekday_imputation', WeekdayImputer(variables=config.model_config.weekday_var)),
-    ##==========Mapper======##
-    ('map_yr',Mapper(config.model_config.yr_var, config.model_config.yr_mappings)),
-    ('map_mnth',Mapper(config.model_config.mnth_var, config.model_config.mnth_mappings )),
-    ('map_season',Mapper(config.model_config.season_var, config.model_config.season_mappings )),
-    ('map_weathersit',Mapper(config.model_config.weathersit_var, config.model_config.weathersit_mappings )),
-    ('map_holiday',Mapper(config.model_config.holiday_var, config.model_config.holiday_mappings )),
-    ('map_workingday',Mapper(config.model_config.workingday_var,config.model_config.workingday_mappings )),
-    ('map_hr',Mapper(config.model_config.hr_var, config.model_config.hr_mappings )),
-    #Outlier handlers
-    ('outlier_temp', OutlierHandler(config.model_config.temp_var)),
-    ('outlier_atemp', OutlierHandler(config.model_config.atemp_var)),
-    ('outlier_hum', OutlierHandler(config.model_config.hum_var)),
-    ('outlier_windspeed', OutlierHandler(config.model_config.windspeed_var)),
-    #weekday OneHotEncoder
-    ('Weekday_OneHot', WeekdayOneHotEncoder()),
-    # scale
-    ('scaler', StandardScaler()),
-    ('regressor', RandomForestRegressor(n_estimators=config.model_config.n_estimators, max_depth=config.model_config.max_depth, random_state=config.model_config.random_state))
-])
+bikeshare_pipe = Pipeline(
+    [
+        (
+            "weathersit_imputation",
+            WeathersitImputer(variables=config.model_config.weathersit_var),
+        ),
+        (
+            "weekday_imputation",
+            WeekdayImputer(variables=config.model_config.weekday_var),
+        ),
+        ##==========Mapper======##
+        ("map_yr", Mapper(config.model_config.yr_var, config.model_config.yr_mappings)),
+        (
+            "map_mnth",
+            Mapper(config.model_config.mnth_var, config.model_config.mnth_mappings),
+        ),
+        (
+            "map_season",
+            Mapper(config.model_config.season_var, config.model_config.season_mappings),
+        ),
+        (
+            "map_weathersit",
+            Mapper(
+                config.model_config.weathersit_var,
+                config.model_config.weathersit_mappings,
+            ),
+        ),
+        (
+            "map_holiday",
+            Mapper(
+                config.model_config.holiday_var, config.model_config.holiday_mappings
+            ),
+        ),
+        (
+            "map_workingday",
+            Mapper(
+                config.model_config.workingday_var,
+                config.model_config.workingday_mappings,
+            ),
+        ),
+        ("map_hr", Mapper(config.model_config.hr_var, config.model_config.hr_mappings)),
+        # Outlier handlers
+        ("outlier_temp", OutlierHandler(config.model_config.temp_var)),
+        ("outlier_atemp", OutlierHandler(config.model_config.atemp_var)),
+        ("outlier_hum", OutlierHandler(config.model_config.hum_var)),
+        ("outlier_windspeed", OutlierHandler(config.model_config.windspeed_var)),
+        # weekday OneHotEncoder
+        ("Weekday_OneHot", WeekdayOneHotEncoder()),
+        # scale
+        ("scaler", StandardScaler()),
+        (
+            "regressor",
+            RandomForestRegressor(
+                n_estimators=config.model_config.n_estimators,
+                max_depth=config.model_config.max_depth,
+                random_state=config.model_config.random_state,
+            ),
+        ),
+    ]
+)
